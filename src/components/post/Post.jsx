0a8c6c92e0bcb5752c141c31d14b1f55876d5a8b
FIANTOSCA <f.iantosca@studenti.unisa.it>
2023-07-07 17:01:28 +0200

    Modifica dei post modifica dei post e del css

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -16,214 +16,108 @@ import NewspaperIcon from '@mui/icons-material/Newspaper';
 import LinkIcon from '@mui/icons-material/Link';
 import Link from '@mui/material/Link';
 
-const Post = ({imageSrcNews, titleNews, nameNews, contentNews, imgContentNews, linkNews, titleContentNews, 
-    imgSourceEvent, titleEvent, nameEvent, nameofEvent, dateEvent, contentEvent,
-    imageProfiloPost, namePost, nameGroupPost, contentPost, imageContentPost, type}) => {
-        
-    if(type ==="News")
-    {
-        return (
-        <Card className="post">
-            <div className='info'>
-                <div className="user-info"> 
-                    
-                <img 
-                className="icona-news"
-                src={imageSrcNews}/>
-
-                    <div className="names">
-                        <h2 className="main-name">{titleNews}</h2>
-                        <h5 className="sec-name">{nameNews}</h5>
-                    </div>
-                </div> 
-                <NewspaperIcon className ='post-type'/>
-            </div>
-
-            <div className='content'>
-                <p>{contentNews}</p>
-            </div>
-
-            <div className="content-imm">
-                <img 
-                className="imm"
-                src={imgContentNews}/> 
-            </div>
-
-            <div className="link-div">
-                <Link href="#" className="link"><LinkIcon className="link-icon"/>{linkNews}</Link>
-                <h4 className="title">{titleContentNews}</h4>
-            </div>
-
-            <div className='interactions'>
-
-                 <IconButton
-                    className="like" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <FavoriteBorderIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-                 
-                 <IconButton
-                    className="commento" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ChatBubbleOutlineIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-
-                 <IconButton
-                    className="save" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <TurnedInNotIcon/>
-                 </IconButton>
-
-                 <IconButton
-                    className="share" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ShareIcon/>
-                 </IconButton>
-
-            </div>
-        </Card>
-    )
+export default function Post  ({imageSrc, title, name, content, imgSrcContent, link, titleNews, nameEvent, date, type}) {
+    
+    
+if(type ==="News")
+{
+    var Avatar = new String("icona-Event");
+    var Follow = new String("followNo");
+    var Info = new String("info-eventNo");
+    var Linki = new String("link-div");
 }
-
 else if (type === "Event")
-    {
-    return (
-        <Card className="post">
-            <div className='info'>
-                <div className="user-info"> 
-                    
-                <img 
-                className="icona-event"
-                src={imgSourceEvent}/>
-
-                    <div className="names">
-                        <h2 className="main-name">{titleEvent}</h2>
-                        <h5 className="sec-name">{nameEvent}</h5>
-                    </div>
-                </div> 
-                <EventIcon className ='post-type'/>
-            </div>
-
-            <div className="info-event">
-             <h4 className="name-event">{nameofEvent}</h4>
-             <h4 className="date">{dateEvent}</h4>
-            </div>
-
-            <div className='content'>
-                <p className="text-content">{contentEvent}</p>
-            </div>
-
-            <div className='follow'>
-             <Button variant="outlined" className="follow-button">Partecipa all'evento</Button>
-            </div>
-
-
-            <div className='interactions'>
-                 <IconButton
-                    className="like" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <FavoriteBorderIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-                 
-                 <IconButton
-                    className="commento" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ChatBubbleOutlineIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-
-                 <IconButton
-                    className="save" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <TurnedInNotIcon/>
-                 </IconButton>
-
-                 <IconButton
-                    className="share" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ShareIcon/>
-                 </IconButton>
-
-            </div>
-        </Card>
-    )
+{
+    var Avatar = new String("icona-Event");
+    var Follow = new String("follow");
+    var Info = new String("info-event");
+    var Linki = new String("link-divNo");
 }
 else if(type === "Post")
 {
-    return (
-        <Card className="post">
-            <div className='info'>
-                <div className="user-info"> 
-                    
-                <img 
-                className="icona-profilo"
-                src={imageProfiloPost}/>
-
-                    <div className="names">
-                        <h2 className="main-name">{namePost}</h2>
-                        <h5 className="sec-name">{nameGroupPost}</h5>
-                    </div>
-                </div> 
-                <ImageIcon className ='post-type'/>
-            </div>
-
-            <div className='content'>
-                <p>{contentPost}</p>
-            </div>
-
-            <div className="content-imm">
-                <img 
-                className="imm"
-                src={imageContentPost}/> 
-            </div>
-
-            <div className='interactions'>
-
-                 <IconButton
-                    className="like" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <FavoriteBorderIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-                 
-                 <IconButton
-                    className="commento" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ChatBubbleOutlineIcon/>
-                    <h5 className="zero">0</h5>
-                 </IconButton>
-
-                 <IconButton
-                    className="save" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <TurnedInNotIcon/>
-                 </IconButton>
-
-                 <IconButton
-                    className="share" 
-                    onClick={() => {// Gestisci il clic sull'icona qui
-                    }}>
-                    <ShareIcon/>
-                 </IconButton>
-
-            </div>
-        </Card>
-    )
- }
+    var Avatar = new String("icona-profilo");
+    var Follow = new String("followNo");
+    var Info = new String("info-eventNo");
+    var Linki = new String("link-divNo");
 }
+ return (
+    <Card className="post">
+        <div className='info'>
+            <div className="user-info"> 
+                
+            <img 
+            className={Avatar}  /*icona-profilo per il post/ icona-Event per Evento/News */
+            src={imageSrc}/>
 
-export default Post
+                <div className="names">
+                    <h2 className="main-name">{title}</h2>
+                    <h5 className="sec-name">{name}</h5>
+                </div>
+            </div> 
+            
+            <NewspaperIcon className ='post-type'/>
+            <EventIcon className ='post-type'/>
+            <ImageIcon className ='post-type'/>
+
+        </div>
+
+        <div className={Info}>
+         <h4 className="name-event">{nameEvent}</h4>
+         <h4 className="date">{date}</h4>
+        </div>
+
+        <div className='content'>
+            <p>{content}</p>
+        </div>
+
+        <div className="content-imm">
+            <img 
+            className="imm"
+            src={imgSrcContent}/> 
+        </div>
+
+        <div className={Follow}> 
+         <Button variant="outlined" className="follow-button">Partecipa all'evento</Button>
+        </div>
+
+        <div className={Linki}>
+            <Link href="#" className="link"><LinkIcon className="link-icon"/>{link}</Link>
+            <h4 className="title">{titleNews}</h4>
+        </div>
+
+        <div className='interactions'>
+
+             <IconButton
+                className="like" 
+                onClick={() => {// Gestisci il clic sull'icona qui
+                }}>
+                <FavoriteBorderIcon/>
+                <h5 className="zero">0</h5>
+             </IconButton>
+             
+             <IconButton
+                className="commento" 
+                onClick={() => {// Gestisci il clic sull'icona qui
+                }}>
+                <ChatBubbleOutlineIcon/>
+                <h5 className="zero">0</h5>
+             </IconButton>
+
+             <IconButton
+                className="save" 
+                onClick={() => {// Gestisci il clic sull'icona qui
+                }}>
+                <TurnedInNotIcon/>
+             </IconButton>
+
+             <IconButton
+                className="share" 
+                onClick={() => {// Gestisci il clic sull'icona qui
+                }}>
+                <ShareIcon/>
+             </IconButton>
+
+        </div>
+    </Card>
+)
+}
