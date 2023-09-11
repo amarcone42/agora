00095f0de92e8e5c91f50e583c8c53f7fafe7f39
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2023-09-11 17:39:25 +0200

    Login page update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,22 @@ import { Button } from "../lib/materialbutton";
 import { Link, Typography } from "@mui/material";
 
 export default function Login() {
-    const router = useRouter()
-    return (
-      <main>
-        <h1>Login</h1>
-        <Link href="/home"><Button variant="filled">Accedi</Button></Link>
-      </main>
-    )
+  const router = useRouter()
+  return (
+    <main id="login-page">
+      <div id="company-section">
+        <img id="company-logo" src="agora-hires-logo.svg" alt="Agorà logo" />
+      </div>
+      <div id="account-section">
+        <h1 class="xxl-text">La voce di tutti</h1>
+        <h2>Iscriviti ora</h2>
+        <Link href="/home"><Button variant="filledTonal">Iscriviti con Google</Button></Link>
+        <Link href="/home"><Button variant="filledTonal">Iscriviti con Apple</Button></Link>
+        <Link href="/home"><Button variant="filled">Crea account</Button></Link>
+        <hr />
+        <h2>Hai già un account?</h2>
+        <Link href="/home"><Button variant="outlined">Accedi</Button></Link>
+      </div>
+    </main>
+  )
 }
