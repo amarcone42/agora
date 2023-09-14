f08e238e424bb8f761cbadbb493180eed2ec5ac9
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2023-09-14 19:19:13 +0200

    Added svg filter to change color and minor changes Edited global.scss to add a filter to adapt the svg color. Added guest.scss with the style of the guest page from global.scss. Edited layout.jsx to import the new stylesheet.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './globals.scss';
+import './guest.scss';
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="it">
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
